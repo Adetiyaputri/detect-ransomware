1d470b8cce6423e6bae348f8eb7d782b74d69cae
Adetiya Putri <adetiyaputri4166@gmail.com>
2025-01-29 16:02:43 +0700

    Update app.py, add Procfile, and create requirements.txt

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def upload_file():
         return render_template('umum.html', result=characteristics)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
